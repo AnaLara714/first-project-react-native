a362aceb79f37e052197b830d43d874f70f88fd7
AnaLara714 <Analara714@gmail.com>
2023-07-20 15:56:53 -0300

    6 - Renderizacao seletiva

--- ProductList.js
+++ ProductList.js
@@ -1,34 +1,34 @@
 import React from "react";
 import ProductCard from "./ProductCard";
-import { Content } from "native-base";
+import { Content, H1 } from "native-base";
 
 export default function ProductList() {
   const getProducts = () => {
-    return [
-      {
-        imagemUrl:"https://loremflickr.com/150/150?random=1",
-        productName: "product 1",
-        releasedDate: "May 31, 2016",
-          description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          rating: 4,
-          numOfReviews: 2
-      },
-      {
-        imagemUrl:"https://loremflickr.com/150/150?random=2",
-        productName: "product 2",
-        releasedDate: "October 31, 2016",
-          description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          rating: 2,
-          numOfReviews: 12
-      },
-      {
-        imagemUrl:"https://loremflickr.com/150/150?random=3",
-        productName: "product 3",
-        releasedDate: "july 31, 2016",
-          description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          rating: 5,
-          numOfReviews: 2
-      }]
+    return []
+      // {
+      //   imagemUrl:"https://loremflickr.com/150/150?random=1",
+      //   productName: "product 1",
+      //   releasedDate: "May 31, 2016",
+      //     description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      //     rating: 4,
+      //     numOfReviews: 2
+      // },
+      // {
+      //   imagemUrl:"https://loremflickr.com/150/150?random=2",
+      //   productName: "product 2",
+      //   releasedDate: "October 31, 2016",
+      //     description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      //     rating: 2,
+      //     numOfReviews: 12
+      // },
+      // {
+      //   imagemUrl:"https://loremflickr.com/150/150?random=3",
+      //   productName: "product 3",
+      //   releasedDate: "july 31, 2016",
+      //     description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      //     rating: 5,
+      //     numOfReviews: 2
+      // }]
   }
 
   const products = getProducts();
@@ -37,7 +37,8 @@ export default function ProductList() {
 
   return (
    <Content>
-     {listProducts}
+     {listProducts.length > 0 ? (<Content>{listProducts}</Content>) : 
+     (<H1>No Products to display</H1>)}
    </Content>
   );
 }
